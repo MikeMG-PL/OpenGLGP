ef98f360ce161e84edbf5bb1fc6095ab91f77c9e
MikeMG-PL <galinski.06@vp.pl>
2023-10-01 20:56:12 +0200

    Element Buffer Object rendering added

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,13 @@ int main(int, char**)
 	GLfloat vertices[] = {
 						-0.5f, -0.5f, 0.0f,
 						 0.5f, -0.5f, 0.0f,
-						 0.0f,  0.5f, 0.0f
+						 0.5f,  0.5f, 0.0f,
+						 -0.5f,  0.5f, 0.0f
+	};
+
+	GLuint indices[] = {
+		0, 1, 2,
+		2, 0, 3
 	};
 
 	// Get vertex shader source
@@ -165,17 +171,23 @@ int main(int, char**)
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLogl);
 	}
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
@@ -243,7 +255,7 @@ int main(int, char**)
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwMakeContextCurrent(window);
 		glfwSwapBuffers(window);
